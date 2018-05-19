7e924b5c6d83384d1b4a54d62b41abad692088ac
brooknew <brooknew@163.com>
2018-05-19 14:56:14 +0800

    mnist hand writing recognize tensorflow code

---
+++ fc1/mnist_test.py
@@ -0,0 +1,70 @@
+import tensorflow as tf
+
+INPUT_NODE = 784
+OUTPUT_NODE = 10
+LAYER1_NODE = 500
+
+def get_weight(shape, regularizer):
+    w = tf.Variable(tf.truncated_normal(shape,stddev=0.1))
+    if regularizer != None: tf.add_to_collection('losses', tf.contrib.layers.l2_regularizer(regularizer)(w))
+    return w
+
+
+def get_bias(shape):  
+    b = tf.Variable(tf.zeros(shape))  
+    return b
+	
+def forward(x, regularizer):
+    w1 = get_weight([INPUT_NODE, LAYER1_NODE], regularizer)
+    b1 = get_bias([LAYER1_NODE])
+    y1 = tf.nn.relu(tf.matmul(x, w1) + b1)
+
+    w2 = get_weight([LAYER1_NODE, OUTPUT_NODE], regularizer)
+    b2 = get_bias([OUTPUT_NODE])
+    y = tf.matmul(y1, w2) + b2
+    return y#coding:utf-8
+import time
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+import mnist_forward
+import mnist_backward
+TEST_INTERVAL_SECS = 5
+
+def test(mnist):
+    with tf.Graph().as_default() as g:
+        x = tf.placeholder(tf.float32, [None, mnist_forward.INPUT_NODE])
+        y_ = tf.placeholder(tf.float32, [None, mnist_forward.OUTPUT_NODE])
+        y = mnist_forward.forward(x, None)
+
+        ema = tf.train.ExponentialMovingAverage(mnist_backward.MOVING_AVERAGE_DECAY)
+        ema_restore = ema.variables_to_restore()
+        saver = tf.train.Saver(ema_restore)
+		
+        correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+        accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+        while True:
+            with tf.Session() as sess:
+                ckpt = tf.train.get_checkpoint_state(mnist_backward.MODEL_SAVE_PATH)
+                print( mnist_backward.MODEL_SAVE_PATH )
+                print( ckpt )
+                print( ckpt.model_checkpoint_path ) 
+                if ckpt and ckpt.model_checkpoint_path:
+                    saver.restore(sess, ckpt.model_checkpoint_path)
+                    global_step = ckpt.model_checkpoint_path.split('/')[-1].split('-')[-1]
+                    accuracy_score = sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels})
+                    print("After %s training step(s), test accuracy = %g" % (global_step, accuracy_score))
+                else:
+                    print('No checkpoint file found')
+                    return
+            time.sleep(TEST_INTERVAL_SECS)
+
+def main():
+    old_v = tf.logging.get_verbosity()
+    tf.logging.set_verbosity(tf.logging.ERROR)
+    mnist = input_data.read_data_sets("./data/", one_hot=True)
+    test(mnist)
+    tf.logging.set_verbosity(old_v)
+
+if __name__ == '__main__':
+    main()
