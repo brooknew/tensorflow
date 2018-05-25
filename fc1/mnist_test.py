cbdbae4cdcaddd21bda66ab0bb5e99753f9fc0eb
brooknew <brooknew@163.com>
2018-05-25 13:19:17 +0800

    course mnist

--- fc1/mnist_test.py
+++ fc1/mnist_test.py
@@ -5,6 +5,13 @@ from tensorflow.examples.tutorials.mnist import input_data
 import mnist_forward
 import mnist_backward
 TEST_INTERVAL_SECS = 5
+"""
+{'Variable/ExponentialMovingAverage':  <tf.Variable 'Variable:0' shape=(784, 500) dtype=float32_ref>,
+ 'Variable_3/ExponentialMovingAverage': <tf.Variable 'Variable_3:0' shape=(10,) dtype=float32_ref>,
+ 'Variable_2/ExponentialMovingAverage': <tf.Variable 'Variable_2:0' shape=(500, 10) dtype=float32_ref>,
+ 'Variable_1/ExponentialMovingAverage': <tf.Variable 'Variable_1:0' shape=(500,) dtype=float32_ref>
+ }
+"""
 
 def test(mnist):
     with tf.Graph().as_default() as g:
@@ -14,6 +21,7 @@ def test(mnist):
 
         ema = tf.train.ExponentialMovingAverage(mnist_backward.MOVING_AVERAGE_DECAY)
         ema_restore = ema.variables_to_restore()
+        print( ema_restore )
         saver = tf.train.Saver(ema_restore)
 		
         correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
@@ -24,6 +32,9 @@ def test(mnist):
                 ckpt = tf.train.get_checkpoint_state(mnist_backward.MODEL_SAVE_PATH)
                 if ckpt and ckpt.model_checkpoint_path:
                     saver.restore(sess, ckpt.model_checkpoint_path)
+                    print ( ckpt.model_checkpoint_path)
+                    print( ckpt.model_checkpoint_path.split('/')[-1] )
+                    print(ckpt.model_checkpoint_path.split('/')[-1].split('-')[-1])
                     global_step = ckpt.model_checkpoint_path.split('/')[-1].split('-')[-1]
                     accuracy_score = sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels})
                     print("After %s training step(s), test accuracy = %g" % (global_step, accuracy_score))
@@ -33,6 +44,7 @@ def test(mnist):
             time.sleep(TEST_INTERVAL_SECS)
 
 def main():
+    tf.logging.set_verbosity(tf.logging.ERROR)
     mnist = input_data.read_data_sets("./data/", one_hot=True)
     test(mnist)
 
